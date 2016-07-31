d2405c6e514dac308f4a5c386614b77cb4a2e782
Rafael Chicoli <rafaelchicoli@hotmail.com>
2016-07-31 13:19:16 +0200

    do not delete logstream

--- router/pump.go
+++ router/pump.go
@@ -331,7 +331,7 @@ func (cp *containerPump) send(msg *Message) {
 		select {
 		case logstream <- msg:
 		case <-time.After(time.Second * 1):
-			debug("pump.send(): send timeout, closing")
+			debug("pump.send(): DO NOT send timeout, closing")
 			// normal call to remove() triggered by
 			// route.Closer() may not be able to grab
 			// lock under heavy load, so we delete here
